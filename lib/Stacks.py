4be71c067594103d8a7a6db0d2ee99d33350bf3f
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-08-27 22:03:44 +0300

    completed stacks

---
+++ lib/Stacks.py
@@ -0,0 +1,18 @@
+def is_balanced(expression):
+    stack = []
+    brackets_map = {')': '(', '}': '{', ']': '['}
+
+    for exp in expression:
+        if exp in '({[':
+            stack.append(exp)
+        elif exp in ')}]':
+            if not stack or stack[-1] != brackets_map[exp]:
+                return False
+            stack.pop()
+
+    return not stack
+
+expression1 = "([]{})"
+expression2 = "([)]"
+print(is_balanced(expression1))  # Output: True
+print(is_balanced(expression2))  # Output: False
